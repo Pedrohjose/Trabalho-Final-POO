bc467eeab1e748334cd5502f7cb81a6a749643c1
carlosisenseee <carlosalfredoisensee@gmail.com>
2025-11-18 14:00:34 -0300

    Arrumando o projeto

---
+++ src/test/EntradaTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class EntradaTest {
+    
+}
